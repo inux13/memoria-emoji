63e2bfbab6ce2a259842b2f7219687dfbb973b03
Mayara <mayarapedrazoli@gmail.com>
2024-12-08 23:53:23 -0300

    Projeto finalizado

---
+++ src/scripts/engine.js
@@ -0,0 +1,58 @@
+const emojis = [
+    "☢",
+    "☢",
+    "👁‍🗨",
+    "👁‍🗨",
+    "🏳‍🌈",
+    "🏳‍🌈",
+    "💅",
+    "💅",
+    "💀",
+    "💀",
+    "🧎‍♀️",
+    "🧎‍♀️",
+    "🤠",    
+    "🤠",
+    '😫',
+    '😫',
+ ];
+let openCard = [];
+
+let shuffleEmojis = emojis.sort(()=>(Math.random() > 0.5 ? 2: -1));
+
+for (let i =0; i < emojis.length; i++) 
+{
+    let box =  document.createElement("div");
+    box.className = "item";
+    box.innerHTML = shuffleEmojis[i];
+    box.onclick = handleClick;
+    document.querySelector(".game").appendChild(box);
+}
+
+function handleClick () {
+    if (openCard.length < 2)
+        this.classList.add("boxOpen");
+    openCard.push(this)
+
+    if(openCard.length == 2) {
+        setTimeout(checkMatch, 500)
+
+    }
+}
+
+
+function checkMatch() {
+    if (openCard[0].innerHTML === openCard[1].innerHTML) {
+        openCard[0].classList.add("boxMatch");
+        openCard[1].classList.add("boxMatch");
+} else {
+    openCard[0].classList.remove("boxOpen");
+    openCard[1].classList.remove("boxOpen");
+}
+
+openCard = [];
+
+if (document.querySelectorAll(".boxMatch").length === emojis.length) {
+    alert("Você venceu!");
+}
+}
